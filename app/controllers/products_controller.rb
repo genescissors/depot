03195241cc380b5746e9dea650cdea974e7b314e
Genesis <genesis.tadeo@rocketmail.com>
2014-08-30 20:47:40 +0800

    Iteration C1-C5 includes caching

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,11 +28,14 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.html { redirect_to @product,
+                      notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created,
+                      location: @product }
       else
         format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors,
+                      status: :unprocessable_entity }
       end
     end
   end
@@ -42,11 +45,13 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, 
+                      notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors, 
+                      status: :unprocessable_entity }
       end
     end
   end
